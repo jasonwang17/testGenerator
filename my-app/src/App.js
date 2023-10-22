c6ea8e670ac3abf8d83d34fa36fd52abaea8de52
Jason Wang <jasonwang2007@outlook.com>
2023-10-22 13:49:21 -0500

    PWA creation

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> My first PWA </h1>
+      <p> Test? </p>?
+    </div>
+  );
+}
+
+export default App;
