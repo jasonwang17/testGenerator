d71698397ef0847675040daf379c27fce7174cf1
wangwei417 <wangwei417@yahoo.com>
2023-10-22 20:23:37 -0500

    display questions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const PracticeTest = ({ test }) => {
+  return (
+    <div>
+      {test.questions.map((question, index) => (
+        <div key={index}>
+          <p>{question.question}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const test = {
+  "questions": [
+    {
+      "question": "A ladder is leaning against a wall. If the base of the ladder is sliding away from the wall at a rate of 2 feet per second and the top of the ladder is sliding down the wall at a rate of 1 foot per second, how fast is the angle between the ladder and the wall changing when the ladder is 10 feet long?",
+      "options": [
+        "0.15 radians per second",
+        "0.20 radians per second",
+        "0.25 radians per second",
+        "0.30 radians per second"
+      ]
+    },
+    {
+      "question": "A spherical balloon is being inflated with helium. If the radius of the balloon is increasing at a rate of 4 inches per minute, how fast is the volume of the balloon increasing when the radius is 6 inches?",
+      "options": [
+        "32π cubic inches per minute",
+        "48π cubic inches per minute",
+        "64π cubic inches per minute",
+        "96π cubic inches per minute"
+      ]
+    },
+    {
+      "question": "A right circular cone is being filled with water. If the radius of the base is decreasing at a rate of 2 inches per minute and the height is increasing at a rate of 3 inches per minute, how fast is the volume of water in the cone changing when the radius is 4 inches and the height is 6 inches?",
+      "options": [
+        "18π cubic inches per minute",
+        "20π cubic inches per minute",
+        "22π cubic inches per minute",
+        "24π cubic inches per minute"
+      ]
+    }
+  ],
+  "answer_key": {
+    "1": "0.25 radians per second",
+    "2": "96π cubic inches per minute",
+    "3": "22π cubic inches per minute"
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <h1> My first PWA </h1>
-      <p> Test? 3:38pm </p>
+      <h1>Practice Test</h1>
+      <hr />
+      <h2>Test Questions</h2>
+      <PracticeTest test={test} />
     </div>
   );
 }
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,98 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const PracticeTest = ({ test }) => {
+  return (
+    <div>
+      {test.questions.map((question, index) => (
+        <div key={index}>
+          <p>{question.question}</p>
+          {question.graph_problem ? (
+            <div dangerouslySetInnerHTML={{ __html: question.graph_latex }}></div>
+          ) : null}
+          <ul>
+            {Object.entries(question.choices).map(([choice, text]) => (
+              <li key={choice}>
+                <label>
+                  <input
+                    type="radio"
+                    name={`question_${index}`}
+                    value={choice}
+                  />
+                  {text}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const test = {
+  "questions": [
+    {
+      "question": "Find the derivative of the function f(x) = 2x^3 - 4x^2 + 3x - 1.",
+      "choices": {
+        "A": "f'(x) = 6x^2 - 8x + 3",
+        "B": "f'(x) = 6x^2 - 8x - 3",
+        "C": "f'(x) = 6x^2 - 8x + 1",
+        "D": "f'(x) = 6x^2 - 4x + 3"
+      },
+      "correct_answer": "A"
+    },
+    {
+      "question": "Solve the equation ∫(2x^2 - 4x + 3)dx from x = 1 to x = 4.",
+      "choices": {
+        "A": "7",
+        "B": "13",
+        "C": "11",
+        "D": "9"
+      },
+      "correct_answer": "C"
+    },
+    {
+      "question": "What is the limit lim(x -> 0) (sin(3x)/3x)?",
+      "choices": {
+        "A": "1",
+        "B": "0",
+        "C": "3",
+        "D": "2"
+      },
+      "correct_answer": "A"
+    },
+    {
+      "question": "Which of the following represents the graph of y = e^x - 1?",
+      "graph_problem": true,
+      "graph_latex": "\\begin{tikzpicture}\n\\begin{axis}[\n    xlabel=$x$,\n    ylabel=$y$,\n    domain=-2:2,\n    samples=100\n]\n\\addplot[color=blue]{exp(x)-1};\n\\end{axis}\n\\end{tikzpicture}",
+      "choices": {
+        "A": "Graph A",
+        "B": "Graph B",
+        "C": "Graph C",
+        "D": "Graph D"
+      },
+      "correct_answer": "A"
+    },
+    {
+      "question": "What is the value of the second derivative of f(x) = x^4 - 4x^3 + 6x^2 - 2x + 1?",
+      "choices": {
+        "A": "f''(x) = 12x^2 - 24x + 12",
+        "B": "f''(x) = 4x^3 - 12x^2 + 12x - 2",
+        "C": "f''(x) = 12x^3 - 24x^2 + 12x - 2",
+        "D": "f''(x) = 4x^2 - 12x + 6"
+      },
+      "correct_answer": "B"
+    }
+  ]
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <PracticeTest test={test} />
+    </div>
+  );
+}
+
+export default App;
