58ef9ebfb1352bc6dfaccaeff6481852bc732a49
Jason Wang <jasonwang2007@outlook.com>
2023-10-26 20:59:32 -0500

    Css beginnings

--- my-app/src/components/PracticeTest.js
+++ my-app/src/components/PracticeTest.js
@@ -1,7 +1,7 @@
 import React, { useState  } from 'react';
 import TikzGraph from './latex';
 import LaTeXFormula from './mathlatex';
-
+import './App.css';
 
 const PracticeTest = ({ test }) => {
     const [selectedAnswers, setSelectedAnswers] = useState({});
